476df0e6e51ea413d446059c2c20de9aa0d0f99f
William Moreschi <williammoreschi@gmail.com>
2020-05-05 07:41:24 -0300

    Teste Finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { url, techs, title} = request.body;
+  const respository = {
+    id: uuid(),
+    likes:0,
+    title,
+    url,
+    techs,
+  }
+  repositories.push(respository);
+  response.json(respository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {url, techs, title} = request.body;
+  const respository = repositories.find(repo => repo.id === id);
+  if(!respository){
+    return response.status(400).send();
+  }
+  respository.url = url;
+  respository.techs = techs;
+  respository.title = title;
+  return response.json(respository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const respository = repositories.findIndex(repo => repo.id === id);
+  if(respository == -1){
+    return response.status(400).send();
+  }
+  repositories.splice(respository,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const respository = repositories.find(repo => repo.id === id);
+  if(!respository){
+    return response.status(400).send();
+  }
+  respository.likes += 1;
+  return response.json(respository);
 });
 
 module.exports = app;
